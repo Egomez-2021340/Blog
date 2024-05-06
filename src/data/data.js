3b36c11d0969d836509279767eb858d6bf1e6fa6
Egomez-2021340 <egomez-2021340@kinal.edu.gt>
2024-05-06 16:09:04 -0600

    Se termina Data

--- src/data/data.js
+++ src/data/data.js
@@ -2,13 +2,46 @@ const data = [
 	{
 		id: 1,
 		image:
-			'https://samuelkraft.com/_next/image?url=%2Fblog%2Fvanilla-extract-with-next-themes%2Fimage.png&w=1080&q=75',
+			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiDzyhA-lMM6quZ9ugax8mLYV5cyjmSuC9Pik9iUGqtcba8oqS1xi5d3gjaprOIAgZv4dQ_hgjFMxsEOjy71ujk9SQu6GUAuJegRwXBoh3hJ0mSNOhore7AkbSb0wXaYs-wT4dOSjQw0BObQYxnx8Nx0eqeofEpxbJMF6tpUjQWXpNL71kO648TpUk3h4w/w499-h292/Captura%20de%20pantalla%202024-05-06%20144537.png',
 		title: 'Proyecto Final',
 		description: {
-			text:'Proyecto Final, aplicación de Ventas',
+			text:'Proyecto Final, aplicación de Ventas <<< CLICK AQUI',
 			url: 'https://github.com/Egomez-2021340/ProyectoFinalBim1.git'
 		}
+	},
+	{
+		id: 2,
+		image:
+			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhQxZgmd3mrT0-1gWavXFgKpxLt83yhC3reWfnta6sxLCZaaxECu5NVJICxY4oF5KCsVqG02DW9IEbIpotM1Zx4H699sg5yOT-kzRYBW8djuUxcgHQNvSvLB20uFfIYoz8lEzSeBOHaTzVLAgvE9YsUBZJWENzH2efjrSmotI8dbW0QUMfeCyUO-3dXsZI/w473-h270/Captura%20de%20pantalla%202024-05-06%20142654.png',
+		title: 'Agenda Web',
+		description: {
+			text:'Agenda de Contactos y Lista de tareas Web<<< CLICK AQUI',
+			url: 'https://github.com/Egomez-2021340/ProyectoFinalBim1.git'
+		}
+	},
+	{
+		id: 3,
+		image:
+			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEifht_QIrBdeKe0jJYrjknz_JbWvIg-6HUdCJziWKKG-FmvhzlMqDNuLBnreJSnB-diewQC_HBwZxqjp7Lvux470xWEtpM1OdjsNk0ayL914oPYOkdKIi2lwvaJx_34bC8WCZslcoQbGRB7c9ffxW9U8bCbkSOGd0az73jkZvo8lJ-O0aLgrasKqXDdcd8/w464-h222/Captura%20de%20pantalla%202024-05-06%20160156.png',
+		title: 'Gestión Hotelera',
+		description: {
+			text:'Esta aplicación deberá ofrecer funcionalidades completas para la gestión de hotel es, habitaciones y eventos asociados.<<< CLICK AQUI',
+			url: 'https://github.com/luccianni/Sistema_de_gestion_hotelera.git'
+		}
+	},
+
+	{
+		id: 4,
+		image:
+			'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEit8tfKIvKMMS7r3OfjcHhM2P5ozpC1j9V6bHgrCxyJWE3yTb6my7KNdJPYnzIHVM1rqFybWjUO37o4stEIJ3vBz-fmsbAQ2YIxFz2XA4TNdEX6ngi4i5pph4t9WlW57wUktLdXS4e5Az_b08GvaTs05F0Rdd8_Nh0bTvwKMt0x1W4hankigmCvmVmf6PU/w605-h302/Captura%20de%20pantalla%202024-05-06%20160732.png',
+		title: 'Gestor de tareas',
+		description: {
+			text:'Una Aplicación destinada al Gestionamiento y ordenamiento de Tareas<<< CLICK AQUI',
+			url: 'https://github.com/luccianni/gestor_tareas.git'
+		}
 	}
+
+	 
 ];
 
 export default data;
