59d4292755b2a6f55dee97fc6c56006f49ec5b68
Egomez-2021340 <egomez-2021340@kinal.edu.gt>
2024-05-06 15:56:12 -0600

    Se finaliza css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
 import { useState } from 'react';
 import './App.css';
-import { Articlelist, ButtonList } from './components';
+import { ArticleList,  } from './components';
 import data from './data/data';
 
-
 function App() {
+	const allCategories = [
+		'All',
+		...new Set(data.map(article => article.category)),
+	];
 
-  const allCategories = ['All', ...new Set(data.map(article => article.category))]
+	const [categories, setCategories] = useState(allCategories);
+	const [articles, setArticles] = useState(data);
 
-  const [categories, setCategories] = useState(allCategories)
-  const [articles, setArticles] = useState(data)
+	const filterCategory = (category) => {
+		if (category === 'All'){
+			setArticles(data)
+			return
+		}
 
+		const filteredData = data.filter(article => article.category === category);
+		setArticles(filteredData)
+	}
 
-const filterCategory = (category) => {
-  console.log(category);
+	return (
+		<>
+			<div className='title'>
+				<h1>
+					<span>Blog</span> Egomez
+				</h1>
+				<img src="https://artificesware.com/images/contenidos/crm-1.png" alt="image-header"/>
+			</div>
+
+			
+
+			<ArticleList articles={articles}/>
+		</>
+	);
 }
 
-  return (
-    <>
-      <div className="title">
-        <h1> <span> Blog</span> Basics</h1>
-        <img src="https://artificesware.com/images/contenidos/crm-1.png" alt="image-header"/>
-      </div>
-      <h1>Hola Mundo</h1>
-
-      <ButtonList categories={categories}/>
-
-      <Articlelist articles={articles}/>
-
-    </>
-  );
-}
-
-export default App
+export default App;
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,5 +1,33 @@
-export const Articlelist = () => {
+import React from 'react';
+
+export const ArticleList = ({ articles }) => {
     return (
-        <div>Articlelist</div>
-    )
-}
+        <div className="article-list">
+            {articles.map(article => (
+                <div className="article-container" key={article.id}>
+                    <div className="img-container">
+                        <img src={article.image} alt={article.title} />
+                    </div>
+                    <div className="article-body">
+                        <h2>{article.title}</h2>
+                        {/* Modificar la parte de la descripción */}
+                        <p>
+                            {/* Si la descripción es un objeto, mostrar el hipervínculo */}
+                            {typeof article.description === 'object' ? (
+                                <a href={article.description.url} target="_blank" rel="noopener noreferrer">
+                                    {article.description.text}
+                                </a>
+                            ) : (
+                                // Si no es un objeto, mostrar el texto normal
+                                article.description
+                            )}
+                        </p>
+                        <div className="article-footer">
+                            <span>{article.ReadingTime}</span>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
